2521768c0863a511f9a5f19b0487cb8a3e25a190
zrh0617 <1298248084@qq.com>
2020-10-09 13:55:41 +0800

    Dialog基本功能

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>
-    Dialog
-  </div>
+  <div>Dialog</div>
+  <h1>示例1</h1>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2"></Dialog>
 </template>
+
+<script lang="ts">
+  import Button from '../lib/Button.vue'
+  import Dialog from '../lib/Dialog.vue'
+  import {
+    ref
+  } from 'vue'
+  export default {
+    components: {
+      Button,
+      Dialog
+    },
+    setup(props) {
+      const x = ref(false)
+      const toggle = () => {
+        x.value = !x.value
+      }
+      const f1 = () => {
+        return false
+      }
+      const f2 = () => {}
+      return {
+        x,
+        toggle,
+        f1,
+        f2
+      }
+    }
+  }
+</script>
