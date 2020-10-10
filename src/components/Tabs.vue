821ef900ba2eab9236e287fe148dccc10095dae7
zrh0617 <1298248084@qq.com>
2020-10-10 11:12:36 +0800

    Tab组件

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -2,9 +2,10 @@
   <div>
     <div>Tabs 示例</div>
     <h1>示例1</h1>
-    <Tabs>
+    <Tabs v-model:selected="x">
       <Tab title="导航1">内容1</Tab>
       <Tab title="导航2">内容2</Tab>
+      <Tab title="导航3">内容3</Tab>
     </Tabs>
   </div>
 </template>
@@ -12,10 +13,17 @@
 <script lang="ts">
   import Tabs from '../lib/Tabs.vue'
   import Tab from '../lib/Tab.vue'
+  import {
+    ref
+  } from 'vue'
   export default {
     components: {
       Tabs,
       Tab
+    },
+    setup() {
+      const x = ref('导航1')
+      return {x}
     }
   }
 </script>
--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    Tab
+    <slot />
   </div>
 </template>
